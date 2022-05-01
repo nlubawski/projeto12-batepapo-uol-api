9e2de2a3527cfe85e32bf8ceeca71f5205620e53
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-05-01 17:51:15 -0300

    criada configuracao pra rodar com nodemon em ambiente de desenvolvimento, utilizada porta 5000 ou outra definina no .env

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ const app = express();
 app.use(cors());
 app.use(json());
 dotenv.config();
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Servidor em pé na porta ${port}`));
